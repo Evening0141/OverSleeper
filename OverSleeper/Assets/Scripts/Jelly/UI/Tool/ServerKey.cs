0eb74f454ba312e6695102d92a1f50154f776953
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2025-05-20 15:01:24 +0900

    datarelay追加

--- OverSleeper/Assets/Scripts/Jelly/UI/Tool/ServerKey.cs
+++ OverSleeper/Assets/Scripts/Jelly/UI/Tool/ServerKey.cs
@@ -3,9 +3,10 @@ using UnityEngine.UI;
 
 public class ServerKey : MonoBehaviour,IChildBehavior
 {
-    [SerializeField] ToolData toolData; // データの取得
     Text levelText;  // レベルを表示するテキスト
     Text moneyText;  // 費用を表示するテキスト
+
+    private float cost;
 
     // インターフェース
     // 設備ボタンの中のサーバー機能です
@@ -18,13 +19,20 @@ public class ServerKey : MonoBehaviour,IChildBehavior
 
     private void Awake()
     {
+        Cost();
         // コンポーネント取得
         Transform child_week = transform.Find("LevelText");
         Transform child_money = transform.Find("moneyText");
         levelText = child_week.GetComponentInChildren<Text>();
         moneyText = child_money.GetComponentInChildren<Text>();
         // 表示
-       // levelText.text = "Lv." + level.ToString();
-       // moneyText.text = "費用:" + money.ToString() + "万";
+        levelText.text = "Lv." + DataRelay.Dr.server.ToString();
+        moneyText.text = "費用:" + cost.ToString() + "万";
+    }
+
+    // コスト計算
+    private void Cost()
+    {
+        cost = (float)DataRelay.Dr.server * 1.3f * 100.0f;
     }
 }
--- OverSleeper/Assets/Scripts/Osho/DataRelay.cs
+++ OverSleeper/Assets/Scripts/Osho/DataRelay.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class DataRelay : MonoBehaviour
 {
-    public int money; //資金用の変数
-    public int maintain; //維持費用の変数
-    public int year; //年の変数
-    public int week; //週の変数
-    public int user; //ユーザー数の変数
-    public int famous; //人気度の変数
-    public int popular;//知名度の変数
+    public int money = 0; //資金用の変数
+    public int maintain = 0; //維持費用の変数
+    public int year = 0; //年の変数
+    public int week = 0; //週の変数
+    public int user = 0; //ユーザー数の変数
+    public int famous = 0; //人気度の変数
+    public int popular = 0;//知名度の変数
 
-    public int server;//サーバーレベル用の変数
-    public int debug;//デバッグ用の変数
-    public int sns;//広告用の変数
+    #region Level
+    public int server = 1;//サーバーレベル用の変数
+    public int debug = 1;//デバッグ用の変数
+    public int sns = 1;//広告用の変数
+    #endregion
 
     //シングルトン用変数
     private static DataRelay dr;
