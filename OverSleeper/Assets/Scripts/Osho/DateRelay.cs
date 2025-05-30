607cd33971b0a6f35d252bc358a527285032dfa4
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2025-05-30 13:35:24 +0900

    ｇｇ

--- OverSleeper/Assets/Scripts/Osho/DateRelay.cs
+++ OverSleeper/Assets/Scripts/Osho/DateRelay.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 public class DateRelay
 {
     private float timer;
-    private const int MONTH_MAX = 12;　//1年（12か月）の定数宣言
-    public float DATE_COOLTIME = 5.0f; //Dateのクールタイム
-
+    //1年（12か月）の定数宣言
+    private const int MONTH_MAX = 12;
+    //Dateのクールタイム
+    public float DATE_COOLTIME = 5.0f; 
    
     public void DateGrow()
     {
         timer += Time.deltaTime;
         if(timer >= DATE_COOLTIME)
         {
-            Debug.Log("Date通っている");
             //cooltimeごとに月を1増やす。
             DataRelay.Dr.Month++;
             //12月に到達したら月を1に戻して年を1増やす。
