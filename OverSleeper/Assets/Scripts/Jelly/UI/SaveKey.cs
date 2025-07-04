73bb674fe4ea5be07771a48ab1a9d1938bdcc879
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2025-07-04 10:41:14 +0900

    セーブした際に視覚的に表示されるUIの追加

--- OverSleeper/Assets/Scripts/Jelly/UI/SaveKey.cs
+++ OverSleeper/Assets/Scripts/Jelly/UI/SaveKey.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 public class SaveKey : MonoBehaviour,IChildBehavior
 {
     [SerializeField] private SaveJSON saveJSON;
+    [SerializeField] private Animator anim; // セーブした際にUIで表記する
 
     // インターフェース
     // セーブ機能
     public void Execute()
     {
-        Debug.Log("セーブするよ");
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
 
-        if (saveJSON != null)
+        if (state.IsName("N"))
         {
-            saveJSON.SaveData();
+            Debug.Log("セーブするよ");
+
+            if (saveJSON != null)
+            {
+                anim.Play("DOWN");
+                saveJSON.SaveData();
+            }
+            else
+            {
+                Debug.LogError("SaveJSONが未設定だよ");
+            }
         }
+        // セーブ出来ない場合のSEだけ鳴らします
         else
         {
-            Debug.LogError("SaveJSONが未設定だよ");
+
         }
     }
 }
