d0b70657e39c9c10d5053852d6ba94e7505639fc
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2025-07-08 10:15:06 +0900

    BANしたときのメッセージ

--- OverSleeper/Assets/Scripts/Jelly/UI/BANKEY.cs
+++ OverSleeper/Assets/Scripts/Jelly/UI/BANKEY.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BANKEY : MonoBehaviour, IChildBehavior
 {
     // アニメーション設定
     [SerializeField] Animator anim;
     // 対象のRect
     [SerializeField] RectTransform rct;
+    // テキスト表示
+    [SerializeField] Text text;
     //オブジェクト座標を元に次のアニメーションが可能かチェック
     const float posY = 550.0f;
 
@@ -16,6 +18,9 @@ public class BANKEY : MonoBehaviour, IChildBehavior
     private const float _TIME = 5.0f;
     private float _timer = _TIME;
 
+    // このスクリプトでのみ扱うタイミング
+    private bool localCheat = false;
+
 
     // インターフェース
     // BAN機能
@@ -24,27 +29,25 @@ public class BANKEY : MonoBehaviour, IChildBehavior
         if (rct.anchoredPosition.y >= posY&&CanBan)
         {
             CanBan = false;
-
+            localCheat = false; // 初期
+            text.text = "修正中"; // 初期テキスト
             Debug.Log("BAN起動！");
             anim.Play("CLOSE");
 
             // チートのチェック
             if (DataRelay.Dr.IsCheat)
             {
-                Debug.Log("いたぞ！");
-                // チートの解除
-                DataRelay.Dr.IsCheat = false;
+                localCheat = true;
             }
             else // いない場合の処理
             {
-                Debug.Log("誤BANかよ");
-                DataRelay.Dr.Money = DataRelay.Dr.Money / 2; // 半額に
-                DataRelay.Dr.Famous -= 1; // 人気度を一つ落とす
+                localCheat = false;
             }
+            FuncChaet();
         }
         else
         {
-            Debug.Log("デバッグチェック");
+            Debug.Log("デバッグチェックanim");
         }
     }
 
@@ -59,13 +62,32 @@ public class BANKEY : MonoBehaviour, IChildBehavior
     {
         _timer -= Time.deltaTime;
 
-        if(_timer<=0)
+        if (_timer<=0)
         {
             // 再セット
             _timer = _TIME;
             CanBan = true;
 
             anim.Play("OPEN");
+        }
+    }
+
+    // BANした時の処理
+    private void FuncChaet()
+    {
+        if (localCheat)
+        {
+            text.color = Color.red;
+            text.text = "B.A.N";
+            // チートの解除
+            DataRelay.Dr.IsCheat = false;
+        }
+        else
+        {
+            text.color = Color.white;
+            text.text = "失敗";
+            DataRelay.Dr.Money = DataRelay.Dr.Money / 2; // 半額に
+            DataRelay.Dr.Famous -= 1; // 人気度を一つ落とす
         }
     }
 }
